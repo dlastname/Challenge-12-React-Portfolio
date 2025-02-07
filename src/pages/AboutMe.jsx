e7fb0c367e30048510ae2a3f394b09351f966c4c
dlastname <dllorens28@gmail.com>
2025-02-06 21:15:17 -0500

    finished contact form and paragraph for about me

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,16 +1,26 @@
 export default function AboutMe() {
-    return (
-      <div>
-        <h1>About Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Hi! My name is Daniel Llorens, and I truly appreciate you taking the
+        time to visit my React portfolio. I'm currently a student in UCF’s and
+        Ed-X’s Web Development Bootcamp, where I’m honing my skills in front-end
+        and back-end development. Originally, I pursued a business degree, but
+        I’ve always had a deep interest in technology and problem-solving. I
+        believe my background in business gives me strong communication and
+        collaboration skills, which are essential for working effectively in a
+        development team. My goal is to combine these strengths with my growing
+        technical expertise to transition into the tech industry as a software
+        developer, and eventually work my way up to a team leadership role. I
+        built this website to showcase the projects I've developed throughout
+        the bootcamp. Each project represents the skills I’ve learned and the
+        progress I’ve made in my journey as a developer. This portfolio itself
+        is one of those projects—designed and built using React, one of the most
+        powerful and popular JavaScript libraries for modern web development.
+        Thank you again for stopping by! Feel free to check out my work, and
+        don’t hesitate to reach out if you’d like to connect.
+      </p>
+    </div>
+  );
+}
